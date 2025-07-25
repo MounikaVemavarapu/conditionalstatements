6ebc614a45f73a2fbd0db38172bb3053d91aa2e1
MounikaVemavarapu <mounikavemavarapu16@gmail.com>
2025-07-25 23:06:34 +0530

    Add files via upload

---
+++ ram.java
@@ -0,0 +1,14 @@
+class Rams_outing{
+    public static void main(String[] args) {
+        int temp = 50;
+        if(temp>45){
+            System.out.println("Ram should stay home");
+        }
+        else if(temp>30 && temp<=45){
+            System.out.println("he should carry carry water");
+        }
+        else{
+            System.out.println("he can go normally");
+        }
+    }
+}
